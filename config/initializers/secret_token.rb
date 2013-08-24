8b3e5b75236d83c68e3a320a573f85ac68bb6ee2
krillen <okulby@yahoo.com>
2013-08-23 23:17:53 -0400

    Added all files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Vte::Application.config.secret_key_base = '70332cbfdd8afcfc57c3b1e21936b6821a5f8f8d11f327e243d72b5a49899e6f132a572ef1f303cb9a745a3058b705b44b829d9ebd8d9d6c82c02ebba9c5f878'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Vte::Application.config.session_store :cookie_store, key: '_vte_session'
